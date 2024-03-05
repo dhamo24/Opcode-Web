4f19355c716e384466083d13d102011b8f477536
dhamo24 <dhamo.t23@gmail.com>
2024-03-05 17:51:59 +0530

    animation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,18 @@
-import Link from "next/link"
-import ThemeToggle from "./ThemeToggle"
-        export default function Header () {
-        return(
-        <header className="bg-white shadow-lg ">
-        <div className=" px-4 py-6 flex justify-between items-center bg-primary dark:bg-black" >
-            <h1 className="text-2xl font-bold text-white">Opcode</h1>
-            <div className="">
-            <ThemeToggle/>
-            </div>
-            <nav className="space-x-4 ">
-                <Link href="/Service" className="text-white">Service</Link>
-                <Link href="/About" className="text-white">About</Link>
-                <Link href="/Contactus" className="text-white">Contactus</Link>
-            </nav>
-        </div> 
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+
+export default function Header() {
+return (
+    <header className="bg-white shadow-lg">
+    <div className="px-4 py-6 flex justify-between items-center bg-primary dark:bg-black">
+        <h1 className="text-2xl font-bold text-white">Opcode</h1>
+        <ThemeToggle />
+        <nav className="space-x-4">
+        <Link href="/Service" className="text-white link">Service</Link>
+        <Link href="/About" className="text-white link">About</Link>
+        <Link href="/Contactus" className="text-white link">Contactus</Link>
+        </nav>
+    </div>
     </header>
-    )
-    }
+);
+}
