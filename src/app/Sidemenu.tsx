3bf81b1b206959c3cb85ae19c05b05354c0b76b7
dhamo24 <dhamo.t23@gmail.com>
2024-03-08 16:57:10 +0530

    side menu active when scroll

--- src/app/Sidemenu.tsx
+++ src/app/Sidemenu.tsx
@@ -1,19 +1,45 @@
-
-import React from "react";
+"use client"
+import React, { useState, useEffect } from "react";
 
 const SideMenu = () => {
+const [activeSection, setActiveSection] = useState("");
+
+useEffect(() => {
+    const handleScroll = () => {
+    const sections = document.querySelectorAll("section[id]");
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (
+        window.scrollY >= sectionTop - 50 &&
+        window.scrollY < sectionTop + sectionHeight - 50
+        ) {
+        setActiveSection(section.getAttribute("id"));
+        }
+    });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+    window.removeEventListener("scroll", handleScroll);
+    };
+}, []);
+
 return (
     <div className="fixed top-16 h-screen w-64 dark:bg-black bg-white border-r">
     <div className="p-20">
         <ul>
         <li className="mb-4">
-            <a href="#" className="text-black-600 dark:text-white">Services</a>
+            <a href="#home" className={`text-black-600 dark:text-white ${activeSection === "home" ? "font-bold" : "" }`}>Home</a>
         </li>
         <li className="mb-4">
-            <a href="#" className="text-black-600 dark:text-white">About</a>
+            <a href="#service" className={`text-black-600 dark:text-white ${activeSection === "service" ? "font-bold" : "" }`}>Service</a>
         </li>
         <li className="mb-4">
-            <a href="#" className="text-black-600 dark:text-white">Contact</a>
+            <a href="#about" className={`text-black-600 dark:text-white ${ activeSection === "about" ? "font-bold" : "" }`}>About</a>
+        </li>
+        <li className="mb-4">
+            <a href="#contact" className={`text-black-600 dark:text-white ${ activeSection === "contact" ? "font-bold" : ""}`}>Contact</a>
         </li>
         </ul>
     </div>
@@ -22,3 +48,7 @@ return (
 };
 
 export default SideMenu;
+
+
+
+
